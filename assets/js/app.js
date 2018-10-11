a0b6f9213baf60b6640346eab38ca6af44a24ac7
Andy Davis <andydavistx@gmail.com>
2018-10-10 19:13:33 -0500

    made some changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,8 @@
 
 const content = $(".content");
 
-const view = function () {
+const view = function (event) {
+    event.preventDefault();
     content.empty();
     for (let i = 0; i < employeeList.length; i++) {
     const newCard= (`<div class= "card">
@@ -12,4 +13,51 @@ const view = function () {
     content.append(newCard); 
 }
 };
-$("#view").on("click",view);
+const showViewDiv = function(event){
+    event.preventDefault();
+    $("#view").toggleClass("hide");
+
+}
+$("#viewEmployee").on("click", view);
+$("#view").on("click", view);
+
+const Add = function (event) {
+    event.preventDefault();
+    content.empty();
+
+    let name = $("#name").val()
+    let  officeNum = $("#officeNumber").val()
+    let phoneNum = $("#phoneNumber").val()
+    employeeList.push({
+        name: name,
+        officeNum:officeNum,
+        phoneNum: phoneNum,
+    });
+    console.log(employeeList)
+}
+
+const showAddForm = function(event){
+    event.preventDefault();
+    $("#AddEmployee").toggleClass("hide");
+
+}
+
+$("#SubmitEmployee").on("click", Add);
+$("#Add").on("click", showAddForm);
+
+
+
+const verify = function () {
+
+let nameVerify = $("#nameVerify").val()
+console.log(nameVerify)
+}
+
+const showVerifyForm = function(event){
+    event.preventDefault();
+    $("#VerifyEmployee").toggleClass("hide");
+
+}
+
+$("#submitEmployeeVerify").on("click",verify)
+$("#verify").on("click",showVerifyForm);
